ab2bcf50e82dfe7827d58b0896162106b8d6247c
Padraig Gleeson <p.gleeson@gmail.com>
2013-02-27 18:02:29 +0000

    Minor edits

--- src/test/java/org/neuroml/importer/AppTest.java
+++ src/test/java/org/neuroml/importer/AppTest.java
@@ -36,54 +36,11 @@ public class AppTest extends TestCase
 
     public void testApp()
     {
-    	NeuroMLDocument nml2 = new NeuroMLDocument();
-        nml2.setId("SomeCells");
 
-        IzhikevichCell iz1 = new IzhikevichCell();
-        iz1.setId("Izh0");
-        iz1.setV0("-70mV");
-        iz1.setThresh("30mV");
-        iz1.setA("0.02");
-        iz1.setB("0.2");
-        iz1.setC("-50");
-        iz1.setD("2");
-        /*
-        iz1.setIamp("2");
-        iz1.setIdel("100 ms");
-        iz1.setIdur("100 ms");
-        */
-        nml2.getIzhikevichCell().add(iz1);
-        
-    	System.out.println("Completed the NeuroML test, created: "+iz1+"...");
-        assertTrue( true );
-        
-		Dimension current = new Dimension("current");
-		current.setI(1);
-		Dimension current2 = new Dimension("current2");
-		current2.setI(1);
-
-    	System.out.println("Completed the LEMS test, created: "+current2+"...");
+    	System.out.println("Completed the Import test...");
     	
-		/*assertTrue("Dimensions match", current.matches(current2));*/
     }
     
     
-    /*
-     * There probably is a better place for these...
-     */    
-
-    public static File getLemsExamplesDir()
-    {
-        return new File("../org.neuroml.model/src/main/resources/NeuroML2CoreTypes");
-    }
-    
-    public static File getTempDir()
-    {
-	    String tempDirName = System.getProperty("user.dir") + File.separator + "src/test/resources/tmp";
-	    File tempDir = new File(tempDirName);
-	    if (!tempDir.exists())
-	    	tempDir.mkdir();
-	    return tempDir;
-    }
     
 }
