4348d19c9784722dd768be8c83751366be9bf22e
Padraig Gleeson <p.gleeson@gmail.com>
2013-03-11 14:33:54 +0000

    Adding tests on SBML to LEMS conversion

---
+++ src/test/java/org/neuroml/importer/test/SBMLImporterTest.java
@@ -0,0 +1,80 @@
+package org.neuroml.importer.test;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.stream.XMLStreamException;
+
+import org.lemsml.jlems.core.expression.ParseError;
+import org.lemsml.jlems.core.logging.E;
+import org.lemsml.jlems.core.sim.ContentError;
+import org.lemsml.jlems.core.sim.ParseException;
+import org.lemsml.jlems.core.type.BuildException;
+import org.lemsml.jlems.core.type.Lems;
+import org.lemsml.jlems.core.xml.XMLException;
+import org.lemsml.jlems.io.util.FileUtil;
+import org.lemsml.jlems.io.xmlio.XMLSerializer;
+import org.neuroml.export.Utils;
+import org.neuroml.importer.sbml.SBMLImporter;
+
+import junit.framework.TestCase;
+
+public class SBMLImporterTest extends TestCase {
+
+    String exampleSrcDir = "src/test/resources";
+    /*
+    if (sbmlFile.getName().indexOf("Izh")>=0) len = 140;
+    if (sbmlFile.getName().indexOf("0039")>=0) len = 50;
+    if (sbmlFile.getName().indexOf("00118")>=0) len = 50;
+    if (sbmlFile.getName().indexOf("00184")>=0) len = 1000;*/
+    
+	public void testIzhikevich()  throws ContentError, ParseError, IOException, ParseException, BuildException, XMLException, XMLStreamException {
+
+        File sbmlFile = new File(exampleSrcDir+"/Izhikevich.xml");
+        convertSBMLtoLEMSFile(sbmlFile, 140);
+	}
+
+	public void testBio39()  throws ContentError, ParseError, IOException, ParseException, BuildException, XMLException, XMLStreamException {
+
+        File sbmlFile = new File(exampleSrcDir+"/BIOMD0000000039.xml");
+        convertSBMLtoLEMSFile(sbmlFile, 50);
+	}
+	
+	public void testBio118()  throws ContentError, ParseError, IOException, ParseException, BuildException, XMLException, XMLStreamException {
+
+        File sbmlFile = new File(exampleSrcDir+"/BIOMD0000000118.xml");
+        convertSBMLtoLEMSFile(sbmlFile, 50);
+	}
+	
+	public void testBio184()  throws ContentError, ParseError, IOException, ParseException, BuildException, XMLException, XMLStreamException {
+
+        File sbmlFile = new File(exampleSrcDir+"/BIOMD0000000184.xml");
+        convertSBMLtoLEMSFile(sbmlFile, 1000);
+	}
+
+	public void convertSBMLtoLEMSFile(File sbmlFile, float simDuration) throws ContentError, ParseError, IOException, ParseException, BuildException, XMLException, XMLStreamException {
+
+        System.out.println( "Converting "+sbmlFile.getAbsolutePath()+" from SBML to LEMS" );
+        float dt = (float)(simDuration/10000.0);
+
+        Lems lems = SBMLImporter.convertSBMLToLEMS(sbmlFile, simDuration, dt);
+        lems.resolve();
+        String lemsString  = XMLSerializer.serialize(lems);
+    
+        //E.info("Created: \n"+lemsString);
+        //E.info("Info: \n"+lems.textSummary());
+
+        File testFile = new File(sbmlFile.getParentFile(), sbmlFile.getName().replaceAll(".xml", "")+"_SBML.xml");
+
+        FileUtil.writeStringToFile(lemsString, testFile);
+
+        E.info("Written to: "+ testFile.getCanonicalPath());
+
+        E.info("Loading LEMS file from: "+ testFile.getAbsolutePath());
+
+		Lems lems2 = Utils.loadLemsFile(testFile);
+	}
+		
+		
+
+}
