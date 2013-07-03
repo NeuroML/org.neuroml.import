5a14080b30be68e3b51f367cf0296e013930455b
Padraig Gleeson <p.gleeson@gmail.com>
2013-07-03 15:57:24 +0100

    Update to v1.0.9

--- src/main/java/org/neuroml/importer/Main.java
+++ src/main/java/org/neuroml/importer/Main.java
@@ -4,5 +4,5 @@ package org.neuroml.importer;
 public class Main 
 {
 
-    public static String ORG_NEUROML_IMPORT_VERSION = "1.0.8";
+    public static String ORG_NEUROML_IMPORT_VERSION = "1.0.9";
 }
