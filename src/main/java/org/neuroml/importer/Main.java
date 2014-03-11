b86a910870013c13c3f461064d3ba7597cf0e3e4
Padraig Gleeson <p.gleeson@gmail.com>
2014-03-11 12:38:02 +0000

    Update to v1.3.0 & using latest org.neuroml.model/export with NML v2beta3

--- src/main/java/org/neuroml/importer/Main.java
+++ src/main/java/org/neuroml/importer/Main.java
@@ -4,5 +4,5 @@ package org.neuroml.importer;
 public class Main 
 {
 
-    public static String ORG_NEUROML_IMPORT_VERSION = "1.2.0";
+    public static String ORG_NEUROML_IMPORT_VERSION = "1.3.0";
 }
